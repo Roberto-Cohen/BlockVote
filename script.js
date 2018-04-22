226cfacc15dbff85b5a67a76b42b9bca39f48ca7
name-here <felixjyancey@mac.com>
2018-04-22 14:25:58 -0400

    Working on tabs

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 function goToTab(tab){
 	for(var i = tab+1; i<5; i ++){
-		//document.getElementById("tab"+i).classList.add("disabled");
+		document.getElementById("tab"+i).classList.add("disabled");
 	}
 }
 function jumpToTab(tab){
+	var instance = M.Tabs.getInstance(document.getElementById("tabs"));
 	var tabElement = document.getElementById("tab"+tab);
 	tabElement.classList.remove("disabled");
 	goToTab(tab);
-
+	instance.select("tab"+tab);
 }
