ea4f04b06dd2c07c5ad69353e65c214434ab5259
name-here <felixjyancey@mac.com>
2018-04-22 14:06:58 -0400

    Small change

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 function goToTab(tab){
 	for(var i = tab+1; i<5; i ++){
-		document.getElementById("tab"+i).classList.add("disabled");
+		//document.getElementById("tab"+i).classList.add("disabled");
 	}
 }
 function jumpToTab(tab){
 	var tabElement = document.getElementById("tab"+tab);
 	tabElement.classList.remove("disabled");
 	goToTab(tab);
-	
+
 }
