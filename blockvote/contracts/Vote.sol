02fe3d7335ad4f5e8575a1de69cc730a0bf340c7
gtier <griffin@Griffins-MacBook-Pro.local>
2018-04-22 10:59:13 -0400

    Edit

--- blockvote/contracts/Vote.sol
+++ blockvote/contracts/Vote.sol
@@ -1,1 +1,21 @@
 pragma solidity ^0.4.17;
+
+contract Vote {
+    address[2] public cadidates;
+
+    struct Candidate {
+        bytes32 name;
+        bytes32 party;
+    }
+
+    struct Voter {
+        bytes32 uid;
+        uint candidateId;
+    }
+
+    mapping
+
+    function vote(uint candidateId) public returns (uint) {
+
+    }
+}
